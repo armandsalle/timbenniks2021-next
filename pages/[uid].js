32c5dc92da96fb9f206d10451dda9275cf5d002d
Armand Sallé <armand.salle@gmail.com>
2021-03-09 09:35:08 +0100

    Fix static export

--- pages/[uid].js
+++ pages/[uid].js
@@ -1,29 +1,27 @@
 //pages/[uid].js
-import { Client } from "../prismic-configuration.js";
-import { RichText } from "prismic-reactjs";
+import { Client } from "../prismic-configuration.js"
+import { RichText } from "prismic-reactjs"
 
 const Post = (props) => {
-  return (
-    <h1 className="text-xl">{RichText.asText(props.doc.data.main_title)}</h1>
-  );
-};
+  return <h1 className="text-xl">{RichText.asText(props.doc.data.main_title)}</h1>
+}
 
 export async function getStaticProps({ params }) {
-  const client = Client();
-  const doc = await client.getByUID("home", params.uid);
+  const client = Client()
+  const doc = await client.getByUID("home", params.uid)
 
   return {
     props: {
       doc,
     },
-  };
+  }
 }
 
 export async function getStaticPaths() {
   return {
     paths: [{ params: { uid: "home" } }],
-    fallback: true,
-  };
+    fallback: false,
+  }
 }
 
-export default Post;
+export default Post
--- pages/writings/[uid].js
+++ pages/writings/[uid].js
@@ -1,10 +1,10 @@
 //pages/[uid].js
-import { Client } from "../../prismic-configuration.js";
-import { RichText } from "prismic-reactjs";
-import Prismic from "@prismicio/client";
-import Link from "next/link";
+import { Client } from "../../prismic-configuration.js"
+import { RichText } from "prismic-reactjs"
+import Prismic from "@prismicio/client"
+import Link from "next/link"
 
-const client = Client();
+const client = Client()
 
 const Post = (props) => {
   return (
@@ -15,36 +15,33 @@ const Post = (props) => {
       <h1 className="text-xl">{RichText.asText(props.doc.data.title)}</h1>
       <div>{RichText.render(props.doc.data.content)}</div>
     </>
-  );
-};
+  )
+}
 
 export async function getStaticProps({ params }) {
-  const doc = await client.getByUID("writing", params.uid);
+  const doc = await client.getByUID("writing", params.uid)
 
   return {
     props: {
       doc,
     },
-  };
+  }
 }
 
 export async function getStaticPaths() {
-  const data = await client.query(
-    Prismic.Predicates.at("document.type", "writing"),
-    {
-      orderings: `[my.writing.publication_date desc]`,
-      pageSize: 100,
-    }
-  );
+  const data = await client.query(Prismic.Predicates.at("document.type", "writing"), {
+    orderings: `[my.writing.publication_date desc]`,
+    pageSize: 100,
+  })
 
   const uids = data.results.map((page) => {
-    return { params: { uid: page.uid } };
-  });
+    return { params: { uid: page.uid } }
+  })
 
   return {
     paths: uids,
-    fallback: true,
-  };
+    fallback: false,
+  }
 }
 
-export default Post;
+export default Post
